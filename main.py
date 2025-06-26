bd16352ec6de1a4e2dc8c27748cd10bae7d6f1af
Clirim Alimi <aclirim10@gmail.com>
2025-06-26 22:36:53 +0200

    Comit Pasi kam shtuar funksionet  CRUD

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
-from fastapi import FastAPI  
-
+from fastapi import FastAPI , HTTPException 
+from pydantic import BaseModel
 app = FastAPI()
 
 items = []
 
+class Item(BaseModel):
+    text: str 
+    is_done : bool = False #
+
 @app.get("/")
 def root():
-    return { "Hello " : "Clirim"}
+    return { "message": "Welcome to the FastAPI application to manage your TODO list"}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    if item_id < len(items):
+        items[item_id] = item
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    if item_id < len(items):
+        del items[item_id]
+        return {"message": f"Item {item_id} deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int ) -> str:
-    item = items[item_id]
-    return item
+@app.get("/items", response_model=list[Item])
+def list_items(limit:int =10 ):
+    return items[0:limit]
 
+
+
+@app.get("/items/{item_id}" , response_model=Item)
+def get_item(item_id: int ) -> Item:
+   if item_id < len(items):
+       return items[item_id]
+   else:
+       raise HTTPException (status_code=404, detail=f"Item {item_id} not found" )
