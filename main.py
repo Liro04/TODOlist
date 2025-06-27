dee417a791ca626b6d427b21c5c0dded8b5a7040
Clirim Alimi <aclirim10@gmail.com>
2025-06-27 14:42:05 +0200

    todo list with checkboxes

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-from fastapi import FastAPI , HTTPException 
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from fastapi import FastAPI, HTTPException, Form
+from fastapi.responses import RedirectResponse           
 from pydantic import BaseModel
+
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 items = []
 
 class Item(BaseModel):
-    text: str 
-    is_done : bool = False #
+    text: str
+    is_done: bool = False
+
+@app.post("/add")
+def add_item(request: Request, text: str = Form(...)):
+    items.append(Item(text=text))
+    return RedirectResponse(url="/todo", status_code=303)
+
+@app.get("/todo")
+def read_todo(request: Request):
+    return templates.TemplateResponse("todo.html", {"request": request, "items": items})
 
 @app.get("/")
 def root():
-    return { "message": "Welcome to the FastAPI application to manage your TODO list"}
-
+    return {"message": "Welcome to the FastAPI application to manage your TODO list"}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
@@ -35,14 +49,12 @@ def create_item(item: Item):
     return items
 
 @app.get("/items", response_model=list[Item])
-def list_items(limit:int =10 ):
+def list_items(limit: int = 10):
     return items[0:limit]
 
-
-
-@app.get("/items/{item_id}" , response_model=Item)
-def get_item(item_id: int ) -> Item:
-   if item_id < len(items):
-       return items[item_id]
-   else:
-       raise HTTPException (status_code=404, detail=f"Item {item_id} not found" )
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int) -> Item:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
